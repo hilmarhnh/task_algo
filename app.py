dd5a0a8dce47b0c23c28c3cb8bc2a6aa14986299
hilmarhnh <raihanahhilma@gmail.com>
2025-04-21 01:42:57 +0800

    Your desired commit message here

--- app.py
+++ app.py
@@ -1,3 +1,60 @@
+from flask import Flask, render_template 
+
+app = Flask(__name__)
+
+
+app_name = "My Application name is: anak ke empat "
+
+#1 App Route di flask "hello word"
+@app.route("/")
+def hello_world():
+    return f"<p>Hello, semuanya, apa kabar! anak ke empat </p>"
+
+
+#2 App Route di flask
+@app.route("/aplikasi/")
+def aplikasi():
+    return "<p>Selamat Datanag di Aplikasi Flask</p>"
+
+
+#3 App Route di HTML
+@app.route("/about/")
+def about():
+    return render_template('about_without_bostrapp.html')
+
+#4 App Route dengan HTML with bostrapp
+@app.route("/about-bostrapp/")
+def about_bostrapp():
+    return render_template('about.html')
+
+
+#5 App Route Dinamis
+@app.route("/nama/<string:nama_mahasiswa>/")
+def getnama(hilma_raihanah):
+    return "nama anda adalah {}".format(hilma_raihanah)
+
+#6 App Route ID
+@app.route('/user/<int:user_id>')  # Hanya menerima angka
+def user_id(user_id):
+    return f"User ID: {user_id}"
+
+#7 App Route Variabel Global
+@app.route('/variabel-global/')
+def variabel_global():
+    return f"Welcome anak ke empat"
+
+#8 App Route Dictionary Variabel
+@app.route('/data')
+def data():
+    user = {"name": "Ali", "age": 25, "city": "Jakarta"}
+    return render_template('data.html', user=user)
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
 from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
@@ -43,3 +100,4 @@ def kalkulator():
 if __name__ == "__main__":
     app.run(debug=True)
 
+from flask import Flask, request, render_template, session
